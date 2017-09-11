d2de6ba943adc0828e947652352ed999b99fefbc
Graham Siener <gsiener@pivotal.io>
2017-09-10 21:29:41 -0400

    whitespace

--- forecast.rb
+++ forecast.rb
@@ -3,22 +3,22 @@ class Forecast
   @target
   @commit
   @achievement
-  
+
   def initialize(spreadsheet)
     ww = spreadsheet.worksheets[1]
-    
+
     @current_quarter = ww["E7"].split(" ")[0]
     @target = ww["J9"] # ACV Goal
     @commit = ww["I9"] # ACV Field MC
     @achievement = ww["K9"] # % commit
   end
-  
+
   def to_s
     "In " + @current_quarter + ", we're at " + @achievement + " of our " + @target + " target."
   end
-  
+
   def to_json
     to_s.to_json
   end
-  
+
 end
