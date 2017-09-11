2590d721bc5f49bf31d4804eb0b52af418abe967
Graham Siener <gsiener@pivotal.io>
2017-09-11 15:27:11 -0400

    munge targets to

--- forecast.rb
+++ forecast.rb
@@ -8,9 +8,14 @@ class Forecast
     ww = spreadsheet.worksheets[1]
 
     @current_quarter = ww["E7"].split(" ")[0]
-    @target = ww["J9"] # ACV Goal
-    @commit = ww["I9"] # ACV Field MC
+    @target = thousands_to_millions(ww["J9"]) # ACV Goal
+    @commit = thousands_to_millions(ww["I9"]) # ACV Field MC
     @achievement = ww["K9"] # % commit
+  end
+
+  def thousands_to_millions(thousands)
+    # "$45,123" => "$45M"
+    "$" + (thousands.gsub(/\D/,'').to_i/1000).to_s + "M"
   end
 
   def to_s
